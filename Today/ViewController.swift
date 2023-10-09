01134566194fbc69583e800a42c4d01e3716e221
Kazuo Murakami <wired2wireless4me@gmail.com>
2023-10-09 20:31:24 +0900

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Kazuo Murakami on R 5/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
